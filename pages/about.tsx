b4ad2d45756a15e3fff202a8d4254c3214aa3d78
Daniel <danielmuniz902@gmail.com>
2023-01-10 04:42:01 +0100

    news app first commit

---
+++ pages/about.tsx
@@ -0,0 +1,9 @@
+import PageLayout from "./components/PageLayout";
+
+export default function About(){
+    return (
+        <PageLayout title='NewsApp - About'>
+            <h1>About Page</h1>
+        </PageLayout>
+    )
+}
